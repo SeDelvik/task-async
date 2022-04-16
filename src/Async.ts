d886a23e10f04ef0dc73fcd3aea1c7a1ed3f77ee
SeDelvik <klybnika4749@gmail.com>
2022-04-16 23:01:05 +0300

    1

--- src/Async.ts
+++ src/Async.ts
@@ -3,6 +3,10 @@
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {}, ms);
+        resolve(ms);
+    });
 }
 
 /*
@@ -11,19 +15,10 @@ export function mock(ms: number): Promise<number> {
 export function getData(): Promise<number[]> {
     const result: number[] = [];
 
-    return mock(100)
-        .then((data1) => {
-            result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
-            result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
-            result.push(data3);
-            return result;
-        });
+    return Promise.all([mock(100), mock(200), mock(300)]).then((results) => {
+        results.forEach((data) => result.push(data));
+        return results;
+    });
 }
 
 /*
@@ -32,8 +27,8 @@ export function getData(): Promise<number[]> {
  */
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
-    } catch (err) {
-        return err.message;
+        await Promise.reject('my error');
+    } catch (err: any) {
+        return err;
     }
 }
